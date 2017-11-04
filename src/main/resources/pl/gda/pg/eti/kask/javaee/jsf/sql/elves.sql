8ad41a3effbc54d6ef9ee5d68cce3ef39dc471cd
Michał Martyniak <michal.martyniak@linux.pl>
2017-11-04 20:18:13 +0100

    Cleanup and update sql

--- src/main/resources/pl/gda/pg/eti/kask/javaee/jsf/sql/elves.sql
+++ src/main/resources/pl/gda/pg/eti/kask/javaee/jsf/sql/elves.sql
@@ -5,10 +5,10 @@ insert into LASY (ilosc_drzew) values (111);
 insert into LASY (ilosc_drzew) values (222);
 insert into LASY (ilosc_drzew) values (333);
 
-insert into ELFY (name, liczba_strzal, rodzaj_luku, las_id) values ('Random Elf', 11, 'LONG', 1);
-insert into ELFY (name, liczba_strzal, rodzaj_luku, las_id) values ('Iorweth', 22, 'RECURVE', 1);
-insert into ELFY (name, liczba_strzal, rodzaj_luku, las_id) values ('Legolas', 33, 'COMPOUND', 2);
-insert into ELFY (name, liczba_strzal, rodzaj_luku, las_id) values ('Dmitrij', 44, 'LONG', 3);
+insert into ELFY (imie, liczba_strzal, rodzaj_luku, las_id) values ('Random Elf', 11, 'DREWNIANY', 1);
+insert into ELFY (imie, liczba_strzal, rodzaj_luku, las_id) values ('Iorweth', 22, 'DREWNIANY', 1);
+insert into ELFY (imie, liczba_strzal, rodzaj_luku, las_id) values ('Legolas', 33, 'DREWNIANY', 2);
+insert into ELFY (imie, liczba_strzal, rodzaj_luku, las_id) values ('Dmitrij', 44, 'DREWNIANY', 3);
 
 select * from ELFY;
 select * from LASY;
